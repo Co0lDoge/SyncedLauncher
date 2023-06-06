f8360aac0f9ac59ffe3d78b5afba044a6e9b0c23
CoolDoge <parfenov314ivan@gmail.com>
2023-06-06 23:50:56 +0500

    Add if file exist check, exception catch for unfound shortcut.

--- AppLauncher/Program.cs
+++ AppLauncher/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AppLauncher
 {
@@ -14,18 +15,46 @@ namespace AppLauncher
     {
         static void Main(string[] args)
         {
-            string[] Content = File.ReadAllLines("AppLaunchConfig.txt");
+            const string path = "AppLaunchConfig.txt";
+            if (!File.Exists(path))
+                FailedExit("AppLaunchConfig.txt is not found.");
 
-            for (int i = 0; i < Content.Length; i++) {
-                // Gets delay in seconds and sleeps before launch
-                int Delay = Int32.Parse(Regex.Match(Content[i], @"\(([^)]*)\)").Groups[1].Value) * 1000;
-                Thread.Sleep(Delay);
+            // String containing all text from file
+            string[] content = File.ReadAllLines(path);
 
-                // Starts process
+            for (int i = 0; i < content.Length; i++) {
+
+                // Get delay in seconds and sleeps before launch
+                int delay = Int32.Parse(Regex.Match(content[i], @"\(([^)]*)\)").Groups[1].Value) * 1000;
+                Thread.Sleep(delay);
+
+                // Start process
                 Process proc = new Process();
-                proc.StartInfo.FileName = Regex.Match(Content[i], @"\{([^}]*)\}").Groups[1].Value;
+                proc.StartInfo.FileName = Regex.Match(content[i], @"\{([^}]*)\}").Groups[1].Value;
+                StartProcess(proc, i);
+            }
+        }
+
+        private static void StartProcess(Process proc, int shortcutNum)
+        {
+            try
+            {
                 proc.Start();
-            }    
+            }
+
+            catch (Exception)
+            {
+                FailedExit($"Shortcut {shortcutNum} not found.");
+            }
+        }
+
+        // Call if program failed to run properly
+        private static void FailedExit(string error)
+        {
+            Console.WriteLine(error);
+            Console.WriteLine("Press Any button to continue...");
+            Console.ReadKey();
+            Environment.Exit(-1);
         }
     }
 }
